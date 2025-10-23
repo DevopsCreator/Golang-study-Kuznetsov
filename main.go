f6a70ba4d705aa5e66c2e2e71526067c257de995
Ivan Kuznetsov <ivan.kuznetsov@flant.com>
2025-10-23 22:57:46 +0300

    Add input from user

--- main.go
+++ main.go
@@ -4,23 +4,45 @@ import "fmt"
 
 func main() {
 
-	column := 8
-	line := 8
+	var column int
+	var line int
 
-	for	i := 0;	i < line;	i++ {
+	for {
+		fmt.Print("Enter column:")
+		_, err := fmt.Scanf("%d", &column)
+		if err != nil {
+			fmt.Print("\nYou enter incorret value\nTry again!)\n")
+			var discard string
+			fmt.Scanln(&discard)
+		} else{
+			break
+		}
+	}
+	for{
+		fmt.Print("Enter column:")
+		_, err := fmt.Scanf("%d", &line)
+		if err != nil {
+			fmt.Print("\nYou enter incorret value\nTry again!)\n")
+			var discard string
+			fmt.Scanln(&discard)
+		} else{
+			break
+		}
+	}
+
+	for i := 0; i < line; i++ {
 		if (i % 2) != 0 {
-			for x := 0; x < column; x+=2 {
+			for x := 0; x < column; x += 2 {
 				fmt.Printf(" ")
 				fmt.Printf("#")
 			}
 		} else {
-			for x := 0; x < column; x+=2 {
+			for x := 0; x < column; x += 2 {
 				fmt.Printf("#")
 				fmt.Printf(" ")
 			}
 		}
 		fmt.Printf("\n")
 	}
-
 
 }
